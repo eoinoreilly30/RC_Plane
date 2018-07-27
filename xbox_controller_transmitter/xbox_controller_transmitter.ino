2bcc862e1ee97d4011a97a92a30972aae0ad648c
Eoin <eoinoreilly30@gmail.com>
2018-07-27 22:14:39 +0100

    updated

--- xbox_controller_transmitter/xbox_controller_transmitter.ino
+++ xbox_controller_transmitter/xbox_controller_transmitter.ino
@@ -1,41 +1,67 @@
 #include <Firmata.h>
+#include "Arduino.h"
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
-int servo1 = 3; //  Incoming Firmata data on this pin is handled specially.
+int leftAileron = 3;
+int rightAileron = 4;
+int elevator = 5;
+int motor = 6;
+
+RF24 radio(7, 8); // CNS, CE
+
+const byte addresses[][6] = {"00001", "00002"};
+
+struct dataStruct{
+  int leftAileron, rightAileron, elevator, motor;
+}data;
 
 void setup()
 {
-   Firmata.setFirmwareVersion(0, 1);
-   Firmata.attach(ANALOG_MESSAGE, analogWriteCallback); // Call this function when analog writes are received
-   Firmata.begin(57600);
-   pinMode(LED_BUILTIN, OUTPUT);
+  Firmata.setFirmwareVersion(0, 1);
+  Firmata.attach(ANALOG_MESSAGE, analogWriteCallback); // Call this function when analog writes are received
+  Firmata.begin(57600);
+  
+  radio.begin();
+  radio.setDataRate(RF24_2MBPS);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setChannel(124);
+  radio.openWritingPipe(addresses[1]); // 00001
+  radio.openReadingPipe(1, addresses[0]); // 00002
+  radio.stopListening();
 }
 
 void loop()
 {
-   while(Firmata.available()) { // Handles Firmata serial input
+   while(Firmata.available()) // Handles Firmata serial input
        Firmata.processInput();
-   }
 }
 
-// Called whenever Arduino receives an analog msg thru Firmata
+// Called whenever Arduino receives an analog message through Firmata
 void analogWriteCallback(byte pin, int value)
 {
-   // If data is sent to reserved pin, execute code
-   if(pin == servo1) {
-    if(value == 180){
-    digitalWrite(LED_BUILTIN, HIGH);
-    }
-    else{
-    digitalWrite(LED_BUILTIN, LOW);
-    }
-   } 
-   
+  if(pin == leftAileron) {
+    data.leftAileron = value;
+  }
+
+  else if(pin == rightAileron) {
+    data.rightAileron = value;
+  }
+
+  else if(pin == elevator) {
+    data.elevator = value;
+  }
+
+  else if(pin == motor) {
+    data.motor = value;
+  }
+  
    // Otherwise, just send the pin value to the appropriate pin on the Arduino
-   else {
+  else {
      pinMode(pin,OUTPUT);
      analogWrite(pin, value);
-   }  
+  }
+
+  radio.write(&data, sizeof(data));
 }
-
-
-
